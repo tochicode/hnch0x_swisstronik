2f1aae83f73f23d5cc857fed98a90be3e88cee07
Joe Hnchx <30940217+tochicode@users.noreply.github.com>
2024-09-14 09:16:33 +0000

    Initial commit

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,12 +1,11 @@
 require("@nomicfoundation/hardhat-toolbox");
-require("dotenv").config();
 
 module.exports = {
   solidity: "0.8.19",
   networks: {
     swisstronik: {
       url: "https://json-rpc.testnet.swisstronik.com/",
-      accounts: [`0x${process.env.PRIVATE_KEY}`],
+      accounts: [""],
     },
   },
 };
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,9 +1,11 @@
 const hre = require("hardhat");
 
 async function main() {
-  const contract = await hre.ethers.deployContract("Swisstronik", ["Hello Swisstronik from Happy Cuan Airdrop!!"]);
+  const contract = await hre.ethers.deployContract("TestToken");
+
   await contract.waitForDeployment();
-  console.log(`Swisstronik contract deployed to ${contract.target}`);
+
+  console.log(`Contract address : ${contract.target}`);
 }
 
 main().catch((error) => {
